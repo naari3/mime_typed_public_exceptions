16ad8aeff7e1d35aef747a0fbc7dc7f6eaae3fa0
naari3 <naari.named@gmail.com>
2019-07-02 20:11:26 +0900

    Implemented MimeTypedPublicExceptions

--- lib/mime_typed_public_exceptions.rb
+++ lib/mime_typed_public_exceptions.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
-module MimeTypedPublicExceptions
-  # Your code goes here...
+# This class behaves roughly as ActionDispatch::PublicExceptions.
+# PublicExceptions is a Rack::Middleware for render an error page
+# which static from /public directory but html only.
+# This class when called, render an error page which static
+# from /public directory with not only html but also others.
+class MimeTypedPublicExceptions < ActionDispatch::PublicExceptions
+  private
+
+  def render(status, content_type, _body)
+    ext = content_type.symbol || 'html' # symbol does not represent an extension
+    path = [
+      "#{public_path}/#{status}.#{I18n.locale}.#{ext}",
+      "#{public_path}/#{status}.#{ext}"
+    ].find { |fp| File.exist?(fp) }
+    if path
+      render_format(status, content_type, File.read(path))
+    else
+      [404, { 'X-Cascade' => 'pass' }, []]
+    end
+  end
 end
